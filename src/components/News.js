de53a283f5a622ef196df710c189d78a62372ebc
Aditya Verma <91420991+AdityaVerma32@users.noreply.github.com>
2024-02-29 23:46:35 +0530

    just removed some sensitive info

--- src/components/News.js
+++ src/components/News.js
@@ -55,21 +55,6 @@ export default class News extends Component {
         })
     }
 
-    // fetchMoreData = async () => {
-    //     this.setState({ page: this.state.page +1})
-    //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ced4927a4a5047e882fa1b48a4204e5d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    //     let data = await fetch(url);
-    //     let parsedData = await data.json();
-    //     console.log(parsedData);
-
-    //     this.setState({
-    //         totalPages: parsedData.totalResults,
-    //         article: this.state.article.concat(parsedData.articles),
-    //     })
-    // }
-
-
-
     render() {
 
         return (
